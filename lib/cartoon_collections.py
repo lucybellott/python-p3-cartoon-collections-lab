c5f8877d800f7782a19f4ef156d07e5f883446d5
Lucy <lucybellott@gmail.com>
2023-12-27 17:04:35 -0500

    passed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,34 @@
-def roll_call_dwarves():
-    pass
+dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+cheese = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def roll_call_dwarves(dwarf_names):
+    counter = 1
+    for dwarf in dwarf_names:
+        print(f"{counter}. {dwarf}")
+        counter +=1
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(planeteer_calls):
+    result=[]
+    for planet in planeteer_calls:
+       result.append(f"{planet.capitalize()}!")
+    return result
+
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
+
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food in cheese:
+            return food
+    return None        
+
+
+#roll_call_dwarves(dwarf_names)
+
+summon_captain_planet(planeteer_calls)
